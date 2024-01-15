8e628c7e59aeedd42686d37f88c8f1802dd5f3c0
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-01-15 01:16:35 +0100

    Update item count to quantity in db.json and add CreateTemplate page

---
+++ src/Components/CreateTemplate.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { postData } from '../Services/api';
+
+export function AddTemplate() {
+  const initialState = {
+    title: '',
+    items: [],
+  };
+  const [state, setState] = useState(initialState);
+
+  function handleInputChange(event) {
+    const name = event.target.name;
+
+    setState(state => {
+      return {
+        ...state,
+        [name]: event.target.value
+      };
+    });
+  }
+
+  async function sendData() {
+    try {
+      await postData('templates', state);
+      setState(initialState);
+    }
+    catch (error) {
+      alert(error.message);
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    sendData();
+    alert('Skabelon oprettet');
+  }
+
+  function handleReset(event) {
+    setState(initialState);
+    event.preventDefault();
+  }
+
+  return (
+    <>
+    <h2>Tilføj et skabelon</h2>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Navn på skabelon:
+        <input
+          name="title"
+          type="text"
+          value={state.title}
+          onChange={handleInputChange} />
+      </label>
+      <br />
+      <input type="reset" value="Cancel" onClick={handleReset} /> <input type="submit" value="Opret skabelon" />
+    </form>
+    </>
+  );
+}
